f3c16a10f08961ab4779db06597d9b1e7378a904
Alejandro Piad <alepiad@gmail.com>
2020-09-01 10:54:54 +0200

    Refactoring

--- animeflv/__main__.py
+++ animeflv/__main__.py
@@ -1,19 +1,21 @@
 # coding: utf-8
 
+from tqdm import tqdm
 from selenium.webdriver import Firefox
 import requests
 import bs4
 import json
 import time
 import fire
+from pathlib import Path
 
 
-def main(title: str, start: int = 1, end: int = 1):
+def main(title: str, start: int = 1, end: int = 1, output_path: str = "."):
     for chapter in range(start, end + 1):
-        download(title, chapter)
+        download(title, chapter, output_path)
 
 
-def download(title: str, chapter: int):
+def download(title: str, chapter: int, output_path: str):
     print("Downloading AnimeFLV.net webpage")
     html = requests.get(f"https://www3.animeflv.net/ver/{title}-{chapter}").text
 
@@ -38,7 +40,8 @@ def download(title: str, chapter: int):
     print(url)
 
     print("Opening Firefox (for later...)")
-    driver = Firefox(executable_path="./geckodriver")
+    driver = Firefox(executable_path=Path(__file__).parent / "geckodriver")
+    driver.minimize_window()
 
     print("Getting the URL")
     driver.get(url)
@@ -57,12 +60,25 @@ def download(title: str, chapter: int):
     driver.close()
 
     print("Downloading video")
+    
+    stream = requests.get(video_url, stream=True)
+    total_size = int(stream.headers.get('content-length', 0))
+    path = Path(output_path) / f"{title}-{chapter}.mp4"
 
-    with open(f"{title}-{chapter}.mp4", "wb") as fp:
-        for batch in requests.get(video_url, stream=True).iter_content(
-            chunk_size=32 * 1024
-        ):
-            fp.write(batch)
+    if path.exists():
+        print(f"(!) Overwriting {path}")
+
+    try:
+        with path.open("wb") as f:
+            with tqdm(total=total_size, unit='B', unit_scale=True, unit_divisor=1024) as pbar:
+                for data in stream.iter_content(32*1024):
+                    f.write(data)
+                    pbar.update(len(data))
+
+        return True
+    except:
+        path.unlink()
+        raise
 
 
 if __name__ == "__main__":
